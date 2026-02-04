87d0ba8c582e63d08987976b21a3a72ef06f8b44
Starteya Pais <starteyapais@gmail.com>
2026-02-03 17:28:13 -0700

    Revert hardcoded API URL; use env-based config

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.solicitedspray.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const fetchRoutes = (query) =>
   axios.get(`${API_BASE_URL}/api/routes/search`, { params: { query } });
